63e45eecf55707d9441963a0d9bf5f00a5144dfa
WenzeJin <andyjibo@outlook.com>
2022-08-12 15:28:18 +0800

    Initial commit

---
+++ 2048main.c
@@ -0,0 +1,120 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include "merge.h"
+#define version "0.1"
+
+//主菜单函数
+int showmenu();
+
+//判断游戏是否结束
+int gamecontinue(int (*arr)[4]);
+
+//在地图上生成一个随机数，k为出现4的概率，否则出现2
+int newnum(int (*map)[4], float k);
+
+//打印地图
+int showmap(int (*map)[4]);
+
+//游戏主函数
+void game();
+
+//高分榜主函数
+void scores();
+
+//主函数
+int main(void)
+{
+    printf("Welcome to 2048 Version: 0.1 Powered by Jin_Wenze:\n");
+    system("pause");
+    system("cls");
+    int running = 1;
+    while(running)
+    {
+        int selection = showmenu();
+        if(selection == 3) break;
+        switch(selection)
+        {
+            case 1:
+                game();
+                break;
+            case 2:
+                scores();
+                break;
+            case 3:
+                running = 0;
+                break;
+        }
+    }
+    system("cls");
+    printf("...Powered by Jin_Wenze...\n");
+    system("pause");
+    return 0;
+}
+
+
+int gamecontinue(int (*arr)[4])
+{
+    return 1;
+}
+
+int showmap(int (*map)[4])
+{
+    for(int i = 0; i < 4; i++)
+    {
+        printf("%d\t%d\t%d\t%d\n\n", map[i][0], map[i][1], map[i][2], map[i][3]);
+    }
+}
+
+int showmenu()
+{
+    printf("请输入相应的字母来选择\n");
+    printf("1.开始游戏\n");
+    printf("2.高分榜 (不可用)\n");        //后期通过文件输入输出实现
+    printf("3.退出\n");
+    int x;
+    scanf("%d",&x);                                  //后期加入输入检验
+    return x;
+}
+
+void game()
+{
+    //进行游戏初始化
+    int map[4][4] = {{2,2,4,8},{4,2,4,8},{4,4,2,2},{2,4,2,4}};
+    /*for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            map[i][j] = 0;
+        }
+    }
+    */
+    int move = 0;
+    while(1)
+    {
+        system("cls");
+
+        showmap(map);
+        if(gamecontinue(map) == 0) break;
+        int check = 0;                  //检验操作是否有意义
+        while(check == 0)
+        {
+            printf("输入数字进行操作: 1 向上 2 向下 3 向左 4 向右\n");
+            scanf("%d", &move);             //输入检验待完成
+            check = merge(map, move);
+            if(check == 0) printf("操作无效，");
+        }
+    }
+}
+
+void scores()
+{
+    system("cls");
+    printf("Runing function scores\n");
+}
+
+int newnum(int (*map)[4], float k)
+{
+    srand((unsigned)time(NULL));
+    int count = 0; //用于统计地图中有多少0
+}
---
+++ merge.h
@@ -0,0 +1,323 @@
+#ifndef _Merge_
+#define _Merge_
+
+
+int mergeup(int (*arr)[4]);
+int mergedown(int (*arr)[4]);
+int mergeleft(int (*arr)[4]);
+int mergeright(int (*arr)[4]);
+
+//用于合并每行或每列的函数,第一个参数传入数组,第二个参数传入方向 1:向上 2:向下 3:向左 4:向右
+int merge(int (*arr)[4], int direction)
+{
+    switch(direction)
+    {
+        case 1:
+        return mergeup(arr);
+        case 2:
+        return mergedown(arr);
+        case 3:
+        return mergeleft(arr);
+        case 4:
+        return mergeright(arr);
+    }
+}
+
+int mergeup(int (*arr)[4])
+{
+    int count = 0; //计数移动次数,便于操作是否有效
+    //归并部分
+    for(int j = 0; j < 4; j++)
+    {
+        int target = -1, ti = 0;
+        for(int i = 0; i < 4; i++)
+        {
+            if(arr[i][j] != 0)
+            {
+                if(arr[i][j] == target)
+                {
+                    arr[ti][j] = 2 * arr[ti][j];
+                    arr[i][j] = 0;
+                    target = -1;
+                    ti = 0;
+                    count ++;
+                    i = -1;
+                }
+                else
+                {
+                    target = arr[i][j];
+                    ti = i;
+                }
+            }
+        }
+    }
+
+    //移动部分
+
+    //分三个位置,从特定位置开始,向下数到第一个非0,统计移动次数,进行一次移动,若到最底仍是0 则不移动,且取消后面的移动
+    for(int j = 0; j < 4; j++)
+    {
+        int check = 0;      //用于控制每一位移动操作是否有意义
+        for(int i = 1; i < 4; i++)          //检查第一位移动是否有意义
+        {
+            if(arr[i][j] != 0) check = 1;
+        }
+        if(check == 0) continue;
+        if(arr[0][j] == 0) count ++;
+        while(arr[0][j] == 0)               //进行移动
+        {
+            arr[0][j] = arr[1][j];
+            arr[1][j] = arr[2][j];
+            arr[2][j] = arr[3][j];
+            arr[3][j] = 0;
+        }
+        check = 0;
+        for(int i = 2; i < 4; i++)          //检查第二位移动是否有意义
+        {
+            if(arr[i][j] != 0) check = 1;
+        }
+        if(check == 0) continue;
+        if(arr[1][j] == 0) count ++;
+        while(arr[1][j] == 0)               //进行移动
+        {
+            arr[1][j] = arr[2][j];
+            arr[2][j] = arr[3][j];
+            arr[3][j] = 0;
+        }
+        check = 0;
+        if(arr[3][j] != 0) check = 1;       //检查第三位移动是否有意义
+        if(check == 0) continue;
+        if(arr[2][j] == 0) count ++;
+        while(arr[2][j] == 0)               //进行移动
+        {
+            arr[2][j] = arr[3][j];
+            arr[3][j] = 0;
+        }
+    }
+    return count;
+}
+
+int mergedown(int (*arr)[4])
+{
+    int count = 0; //计数移动次数,便于操作是否有效
+    //归并部分
+    for(int j = 0; j < 4; j++)
+    {
+        int target = -1, ti = 3;
+        for(int i = 3; i >= 0; i--)
+        {
+            if(arr[i][j] != 0)
+            {
+                if(arr[i][j] == target)
+                {
+                    arr[ti][j] = 2 * arr[ti][j];
+                    arr[i][j] = 0;
+                    target = -1;
+                    ti = 3;
+                    count ++;
+                    i = 4;
+                }
+                else
+                {
+                    target = arr[i][j];
+                    ti = i;
+                }
+            }
+        }
+    }
+
+    //移动部分
+
+    //分三个位置,从特定位置开始,向下数到第一个非0,统计移动次数,进行一次移动,若到最底仍是0 则不移动,且取消后面的移动
+    for(int j = 0; j < 4; j++)
+    {
+        int check = 0;      //用于控制每一位移动操作是否有意义
+        for(int i = 2; i >= 0; i--)          //检查第一位移动是否有意义
+        {
+            if(arr[i][j] != 0) check = 1;
+        }
+        if(check == 0) continue;
+        if(arr[3][j] == 0) count ++;
+        while(arr[3][j] == 0)               //进行移动
+        {
+            arr[3][j] = arr[2][j];
+            arr[2][j] = arr[1][j];
+            arr[1][j] = arr[0][j];
+            arr[0][j] = 0;
+        }
+        check = 0;
+        for(int i = 1; i >= 0; i--)          //检查第二位移动是否有意义
+        {
+            if(arr[i][j] != 0) check = 1;
+        }
+        if(check == 0) continue;
+        if(arr[2][j] == 0) count ++;
+        while(arr[2][j] == 0)               //进行移动
+        {
+            arr[2][j] = arr[1][j];
+            arr[1][j] = arr[0][j];
+            arr[0][j] = 0;
+        }
+        check = 0;
+        if(arr[0][j] != 0) check = 1;       //检查第三位移动是否有意义
+        if(check == 0) continue;
+        if(arr[1][j] == 0) count ++;
+        while(arr[1][j] == 0)               //进行移动
+        {
+            arr[1][j] = arr[0][j];
+            arr[0][j] = 0;
+        }
+    }
+    return count;
+}
+
+int mergeleft(int (*arr)[4])
+{
+    int count = 0; //计数移动次数,便于操作是否有效
+    //归并部分
+    for(int i = 0; i < 4; i++)
+    {
+        int target = -1, tj = 0;
+        for(int j = 0; j < 4; j++)
+        {
+            if(arr[i][j] != 0)
+            {
+                if(arr[i][j] == target)
+                {
+                    arr[i][tj] = 2 * arr[i][tj];
+                    arr[i][j] = 0;
+                    target = -1;
+                    tj = 0;
+                    count ++;
+                    j = -1;
+                }
+                else
+                {
+                    target = arr[i][j];
+                    tj = j;
+                }
+            }
+        }
+    }
+
+    //移动部分
+
+    //分三个位置,从特定位置开始,向下数到第一个非0,统计移动次数,进行一次移动,若到最底仍是0 则不移动,且取消后面的移动
+    for(int i = 0; i < 4; i++)
+    {
+        int check = 0;      //用于控制每一位移动操作是否有意义
+        for(int j = 1; j < 4; j++)          //检查第一位移动是否有意义
+        {
+            if(arr[i][j] != 0) check = 1;
+        }
+        if(check == 0) continue;
+        if(arr[i][0] == 0) count ++;
+        while(arr[i][0] == 0)               //进行移动
+        {
+            arr[i][0] = arr[i][1];
+            arr[i][1] = arr[i][2];
+            arr[i][2] = arr[i][3];
+            arr[i][3] = 0;
+        }
+        check = 0;
+        for(int j = 2; j < 4; j++)          //检查第二位移动是否有意义
+        {
+            if(arr[i][j] != 0) check = 1;
+        }
+        if(check == 0) continue;
+        if(arr[i][1] == 0) count ++;
+        while(arr[i][1] == 0)               //进行移动
+        {
+            arr[i][1] = arr[i][2];
+            arr[i][2] = arr[i][3];
+            arr[i][3] = 0;
+        }
+        check = 0;
+        if(arr[i][3] != 0) check = 1;       //检查第三位移动是否有意义
+        if(check == 0) continue;
+        if(arr[i][2] == 0) count ++;
+        while(arr[i][2] == 0)               //进行移动
+        {
+            arr[i][2] = arr[i][2];
+            arr[i][3] = 0;
+        }
+    }
+    return count;
+}
+
+int mergeright(int (*arr)[4])
+{
+    int count = 0; //计数移动次数,便于操作是否有效
+    //归并部分
+    for(int i = 0; i < 4; i++)
+    {
+        int target = -1, tj = 0;
+        for(int j = 3; j < 4; j--)
+        {
+            if(arr[i][j] != 0)
+            {
+                if(arr[i][j] == target)
+                {
+                    arr[i][tj] = 2 * arr[i][tj];
+                    arr[i][j] = 0;
+                    target = -1;
+                    tj = 3;
+                    count ++;
+                    j = -1;
+                }
+                else
+                {
+                    target = arr[i][j];
+                    tj = j;
+                }
+            }
+        }
+    }
+
+    //移动部分
+
+    //分三个位置,从特定位置开始,向下数到第一个非0,统计移动次数,进行一次移动,若到最底仍是0 则不移动,且取消后面的移动
+    for(int i = 0; i < 4; i++)
+    {
+        int check = 0;      //用于控制每一位移动操作是否有意义
+        for(int j = 3; j >= 0; j--)          //检查第一位移动是否有意义
+        {
+            if(arr[i][j] != 0) check = 1;
+        }
+        if(check == 0) continue;
+        if(arr[i][3] == 0) count ++;
+        while(arr[i][3] == 0)               //进行移动
+        {
+            arr[i][3] = arr[i][2];
+            arr[i][2] = arr[i][1];
+            arr[i][1] = arr[i][0];
+            arr[i][0] = 0;
+        }
+        check = 0;
+        for(int j = 2; j >= 0; j--)          //检查第二位移动是否有意义
+        {
+            if(arr[i][j] != 0) check = 1;
+        }
+        if(check == 0) continue;
+        if(arr[i][2] == 0) count ++;
+        while(arr[i][2] == 0)               //进行移动
+        {
+            arr[i][2] = arr[i][1];
+            arr[i][1] = arr[i][0];
+            arr[i][0] = 0;
+        }
+        check = 0;
+        if(arr[i][0] != 0) check = 1;       //检查第三位移动是否有意义
+        if(check == 0) continue;
+        if(arr[i][1] == 0) count ++;
+        while(arr[i][1] == 0)               //进行移动
+        {
+            arr[i][1] = arr[i][0];
+            arr[i][0] = 0;
+        }
+    }
+    return count;
+}
+
+
+#endif
